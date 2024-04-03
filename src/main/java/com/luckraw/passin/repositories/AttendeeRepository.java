4fc6bb52bcf90727f89af11d24a8f4eea5b04b07
Lucas <krawdev@gmail.com>
2024-04-03 10:01:41 -0300

    feat: method findByEventId impl.

--- src/main/java/com/luckraw/passin/repositories/AttendeeRepository.java
+++ src/main/java/com/luckraw/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,9 @@ package com.luckraw.passin.repositories;
 import com.luckraw.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+
+    List<Attendee> findByEventId(String eventId);
 }
