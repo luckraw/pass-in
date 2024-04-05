4960d00e820b5d6bda3b0174939564a63f4b1df1
Lucas <krawdev@gmail.com>
2024-04-05 00:34:22 -0300

    feat: Handlers and Exceptions impl.

--- src/main/java/com/luckraw/passin/config/ExceptionEntityHandler.java
+++ src/main/java/com/luckraw/passin/config/ExceptionEntityHandler.java
@@ -1,6 +1,12 @@
 package com.luckraw.passin.config;
 
+import com.luckraw.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import com.luckraw.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import com.luckraw.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import com.luckraw.passin.domain.event.exceptions.EventFullException;
 import com.luckraw.passin.domain.event.exceptions.EventNotFoundException;
+import com.luckraw.passin.dto.general.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,4 +19,23 @@ public class ExceptionEntityHandler {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventFull(EventFullException exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistException.class)
+    public ResponseEntity handleAttendeeAlreadyExist(AttendeeAlreadyExistException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExist(CheckInAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
 }
---
+++ src/main/java/com/luckraw/passin/dto/general/ErrorResponseDTO.java
@@ -0,0 +1,4 @@
+package com.luckraw.passin.dto.general;
+
+public record ErrorResponseDTO(String message) {
+}
